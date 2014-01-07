be6766f7c9a7b724c138a2424e88e959867fb1ec
Tejp <Tejpbit@hotmail.se>
2014-01-07 21:24:06 +0100

    Test commit to se changes in different branches

---
+++ src/controller/CreateTool.java
@@ -0,0 +1,16 @@
+package controller;
+
+import view.GraphView;
+
+/**
+ * 
+ * @author André
+ * tool to create edges and verticies, also used for moving edges.
+ */
+public class CreateTool extends Tool {
+
+	public CreateTool(GraphView graphMap) {
+		super(graphMap);
+	}
+	
+}
